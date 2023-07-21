4d4b52a20e6b983eae9ee5e3f0414ded9da22e7a
JayFate <37610029@qq.com>
2023-07-21 16:50:22 +0800

    登录组件基本布局
    Signed-off-by: JayFate <37610029@qq.com>

--- vue-heima-admin/src/App.vue
+++ vue-heima-admin/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app">App 根组件</div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
--- vue-heima-admin/src/router/index.js
+++ vue-heima-admin/src/router/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import LoginComp from '../components/LoginComp.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  // 自动跳转到 login
+  { path: '/', redirect: '/login' },
+  { path: '/login', name: 'LoginComp', component: LoginComp }
 ]
 
 const router = new VueRouter({
