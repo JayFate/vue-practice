54412cc4576a42985df13b0db2e5448844b04927
JayFate <37610029@qq.com>
2023-07-23 20:41:14 +0800

    登录之前表单预验证
    Signed-off-by: JayFate <37610029@qq.com>

--- vue-heima-admin/src/components/LoginComp.vue
+++ vue-heima-admin/src/components/LoginComp.vue
@@ -31,7 +31,7 @@
         </el-form-item>
         <!-- 按钮区域 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
+          <el-button @click="login" type="primary">登录</el-button>
           <el-button @click="resetLoginForm" type="info">重置</el-button>
         </el-form-item>
       </el-form>
@@ -64,6 +64,11 @@ export default {
     // 重置表单
     resetLoginForm () {
       this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(valid => {
+        console.log(valid)
+      })
     }
   }
 }
