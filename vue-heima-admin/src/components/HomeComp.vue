7b3958cd9c8669a0fbef4cfbb50630050a1a7ae8
JayFate <37610029@qq.com>
2023-07-25 22:20:45 +0800

    导航菜单el-menu的基本使用
    Signed-off-by: JayFate <37610029@qq.com>

--- vue-heima-admin/src/components/HomeComp.vue
+++ vue-heima-admin/src/components/HomeComp.vue
@@ -8,7 +8,26 @@
       <el-button type="info" @click="logout">登录</el-button></el-header
     >
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区域 -->
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+          <!-- 一级菜单 -->
+          <el-submenu index="1">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>选项1</span>
+              </template></el-menu-item
+            >
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
